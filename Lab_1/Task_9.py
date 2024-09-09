1cf9f75f75832ffdc21917fbacfe1cd06779f1d1
ARTEZON <artezon@yandex.ru>
2024-09-09 10:33:14 +0300

    Lab 1 task 9 completed

---
+++ Lab_1/Task_9.py
@@ -0,0 +1,13 @@
+import cv2
+
+ip_address = '192.168.180.194'
+port = '8080'
+
+video = cv2.VideoCapture(f"http://{ip_address}:{port}/video")
+while True:
+    ok, img = video.read()
+    if not ok or cv2.waitKey(1) & 0xFF == 27:
+        break
+    cv2.imshow(f'Video stream from {ip_address}:{port}', img)
+video.release()
+cv2.destroyAllWindows()
